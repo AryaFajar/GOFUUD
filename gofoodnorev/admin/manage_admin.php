a0a598bad87771d53a3f71b7b62be8199136dd1c
AryaFajar <105540722+AryaFajar@users.noreply.github.com>
2022-11-08 19:53:02 +0700

    Add files via upload

---
+++ gofoodnorev/admin/manage_admin.php
@@ -0,0 +1,27 @@
+<?php include('partials/navbar.php'); ?>
+
+    <!-- main content section starts -->
+    <div class="main-content">
+        <div class="wrapper">
+            <h1>Manage Admin</h1>
+
+            <table>
+                <tr>
+                    <th>ID</th>
+                    <th>Nama_lengkap</th>
+                    <th>Username</th>
+                    <th>Actions</th>
+                </tr>
+
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </table>
+        </div>
+    </div>
+    <!-- main content section ends -->
+
+<?php include('partials/footer.php'); ?>
---
+++ gofoodnorev/admin/manage_food.php
@@ -0,0 +1,10 @@
+<?php include('partials/navbar.php'); ?>
+
+<div class="main-content">
+    <div class="wrapper">
+    <h1>Manage Food</h1>
+    </div>
+    
+</div>
+
+<?php include('partials/footer.php'); ?>
